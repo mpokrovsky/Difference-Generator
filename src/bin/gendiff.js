b1ae051742e6973ac0aa2e935c789b725c15c28e
mpokrovsky <ovmantsurova@edu.hse.ru>
2019-03-26 16:39:26 +0300

    -

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import { version } from '../../package.json';
 import gendiff from '..';
 
 program
-  .version(version)
+  .version('1.0.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
