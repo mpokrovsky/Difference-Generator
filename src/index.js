8c034f341561eec790f9721b22887e9250d1cfc0
mpokrovsky <ovmantsurova@edu.hse.ru>
2019-03-26 17:46:49 +0300

    check

--- src/index.js
+++ src/index.js
@@ -36,26 +36,6 @@ const resFormat = [
 ];
 
 const getResultFormat = (arg, obj1, obj2) => resFormat.find(({ check }) => check(arg, obj1, obj2));
-/*
-export default (filePathBefore, filePathAfter) => {
-  const { process } = getFileExtension(filePathBefore);
-
-  const contentBefore = readFileSync(filePathBefore, 'utf8');
-  const objBefore = process(contentBefore);
-
-  const contentAfter = readFileSync(filePathAfter, 'utf8');
-  const objAfter = process(contentAfter);
-
-  const allKeys = uniq([...Object.keys(objBefore), ...Object.keys(objAfter)]);
-
-  const difference = allKeys.reduce((acc, key) => {
-    const { result } = getResultFormat(key, objBefore, objAfter);
-    return acc + result(key, objBefore, objAfter);
-  }, '');
-
-  return `{\n${difference}}`;
-};
-*/
 
 export default (filePathBefore, filePathAfter) => {
   const { process } = getFileExtension(filePathBefore);
