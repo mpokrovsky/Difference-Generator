96a874e41017f0a01620958be1c17a81808bc808
mpokrovsky <ovmantsurova@edu.hse.ru>
2019-04-01 02:09:35 +0300

    add ast building & piece of render

--- src/index.js
+++ src/index.js
@@ -1,44 +1,66 @@
-import { has, union } from 'lodash';
+import {
+  has, union, isPlainObject,
+} from 'lodash';
 import { readFileSync } from 'fs';
 import path from 'path';
 import parse from './parsers';
 
-const itemChangeStr = [
+const itemLines = {
+  added: (item, depth) => `${'    '.repeat(depth - 1)}  + ${item.key}: ${item.value}`,
+  removed: (item, depth) => `${'    '.repeat(depth - 1)}  - ${item.key}: ${item.value}`,
+  unchanged: (item, depth) => `${'    '.repeat(depth - 1)}    ${item.key}: ${item.value}`,
+  changed: (item, depth) => `${'    '.repeat(depth - 1)}  + ${item.key}: ${item.valueAfter}\n${'    '.repeat(depth - 1)}  - ${item.key}: ${item.valueBefore}`,
+  parent: (item, depth, fun) => `${'    '.repeat(depth)}    ${item.key}: {\n${fun(item.children, depth + 1)}\n}`,
+};
+
+const render = (data, depth = 1) => data.map(item => itemLines[item.type](item, depth, render)).join('\n');
+
+const itemToAst = [
   {
-    check: (key, obj1, obj2) => has(obj1, key) && has(obj2, key) && obj1[key] === obj2[key],
-    result: (key, obj2) => `    ${key}: ${obj2[key]}`,
+    type: 'parent',
+    check: (key, obj1, obj2) => isPlainObject(obj1[key]) && isPlainObject(obj2[key]),
+    result: (key, obj1, obj2, fun) => ({ key, children: fun(obj1[key], obj2[key]) }),
   },
   {
-    check: (key, obj1, obj2) => has(obj1, key) && has(obj2, key) && obj1[key] !== obj2[key],
-    result: (key, obj1, obj2) => `  + ${key}: ${obj2[key]}\n  - ${key}: ${obj1[key]}`,
-  },
-  {
+    type: 'added',
     check: (key, obj1, obj2) => !has(obj1, key) && has(obj2, key),
-    result: (key, obj1, obj2) => `  + ${key}: ${obj2[key]}`,
+    result: (key, obj1, obj2) => ({ key, value: obj2[key] }),
   },
   {
+    type: 'removed',
     check: (key, obj1, obj2) => has(obj1, key) && !has(obj2, key),
-    result: (key, obj1) => `  - ${key}: ${obj1[key]}`,
+    result: (key, obj1) => ({ key, value: obj1[key] }),
+  },
+  {
+    type: 'unchanged',
+    check: (key, obj1, obj2) => obj1[key] === obj2[key],
+    result: (key, obj2) => ({ key, value: obj2[key] }),
+  },
+  {
+    type: 'changed',
+    check: (key, obj1, obj2) => obj1[key] !== obj2[key],
+    result: (key, obj1, obj2) => ({ key, valueBefore: obj1[key], valueAfter: obj2[key] }),
   },
 ];
 
-const getItemChangeStr = (key, objBefore, objAfter) => itemChangeStr
-  .find(({ check }) => check(key, objBefore, objAfter));
+const getItemToAst = (key, objBefore, objAfter, fun) => itemToAst
+  .find(({ check }) => check(key, objBefore, objAfter, fun));
+
+const buildAst = (obj1, obj2) => {
+  const allKeys = union(Object.keys(obj1), Object.keys(obj2));
+  return allKeys.map((key) => {
+    const { type, result } = getItemToAst(key, obj1, obj2, buildAst);
+    return { ...result(key, obj1, obj2, buildAst), type };
+  });
+};
 
 const getContent = filePath => readFileSync(filePath, 'utf8');
-
 const getExtension = filepath => path.extname(filepath);
 
 export default (filePathBefore, filePathAfter) => {
   const objBefore = parse(getContent(filePathBefore), getExtension(filePathBefore));
   const objAfter = parse(getContent(filePathAfter), getExtension(filePathAfter));
 
-  const allKeys = union(Object.keys(objBefore), Object.keys(objAfter));
-
-  const difference = allKeys.reduce((acc, key) => {
-    const { result } = getItemChangeStr(key, objBefore, objAfter);
-    return [...acc, result(key, objBefore, objAfter)];
-  }, []);
-
-  return `{\n${difference.join('\n')}\n}`;
+  const ast = buildAst(objBefore, objAfter);
+  return `{\n${render(ast)}\n}`;
 };
